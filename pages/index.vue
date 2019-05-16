707592fc0fdee582151a22c5ee6853ecca7ea71b
Kengo Suzuki <ksuzuki@folio-sec.com>
2019-05-16 17:34:52 +0200

    nuxt

---
+++ pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <h1>
+    <v-app id="inspire">
+      <div>
+        <v-btn color="info" @click="handleClick">Authorize</v-btn>
+      </div>
+    </v-app>
+  </h1>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  fetch({ store, params }) {
+    return axios.get('http://localhost:8080/').then(res => {
+      store.commit('add', res.data)
+    })
+  }
+}
+</script>
+<script>
+export default {
+  methods: {
+    handleClick: function() {
+      alert('test')
+    }
+  }
+}
+</script>
