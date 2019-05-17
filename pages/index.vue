692e850f338efb5d19ec236d3eae956cb52bd056
Kengo Suzuki <ksuzuki@folio-sec.com>
2019-05-17 07:26:08 +0200

    make vue front and backend work

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,6 @@
 <template>
   <h1>
+    Authorization Code: {{ results }}
     <v-app id="inspire">
       <div>
         <v-btn color="info" @click="handleClick">Authorize</v-btn>
@@ -12,18 +13,27 @@
 import axios from 'axios'
 
 export default {
+  data: function() {
+    return {
+      results: []
+    }
+  },
+  computed: {
+    hoge() {
+      return this.$store.state.albums
+    }
+  },
   fetch({ store, params }) {
     return axios.get('http://localhost:8080/').then(res => {
-      store.commit('add', res.data)
+      // store.commit('add', res.data)
+      // console.log(store.state.albums)
     })
-  }
-}
-</script>
-<script>
-export default {
+  },
   methods: {
     handleClick: function() {
-      alert('test')
+      axios.get('http://localhost:8080/').then(res => {
+        this.results = res.data
+      })
     }
   }
 }
